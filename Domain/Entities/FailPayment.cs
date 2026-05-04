c172cd54c8f51fdccd82a7f3aba111339ca7e283
Sarthak12397 <ssarthak737@gmail.com>
2026-05-04 20:55:03 +0545

    Domain Added

---
+++ Domain/Entities/FailPayment.cs
@@ -0,0 +1,11 @@
+public class FailPayment
+{
+    public Guid PaymentId{get; private set;}
+    public Guid ExternalPaymentId{get; private set;}
+    public Guid ExternalInvoiceId{get; private set;
+    
+    
+    
+    
+    }
+}
